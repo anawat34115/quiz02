ae87b44968174f5793ce0235edc31b0532c1ea69
anawat34115 <85146954+anawat34115@users.noreply.github.com>
2023-04-03 10:23:55 +0700

    Add files via upload
    add file

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿class Program {
+    static void Main(string[] args) {
+        string input;
+        double number;
+        double sum = 0;
+        double count = 0;
+        double max = double.MinValue;
+        double min = double.MaxValue;
+
+        while (true) {
+            Console.Write("Enter a number (or 'End' to stop): ");
+            input = Console.ReadLine();
+
+            if (input == "End") {
+                break;
+            }
+
+            if (!double.TryParse(input, out number)) {
+                continue;
+            }
+
+            sum += number;
+            count++;
+            if (number > max) {
+                max = number;
+            }
+            if (number < min) {
+                min = number;
+            }
+        }
+
+        Console.Write("Enter mode of statistical operation (FindMax, FindMin, FindMean): ");
+        string mode = Console.ReadLine();
+
+        switch (mode) {
+            case "FindMax":
+                Console.WriteLine(max);
+                break;
+            case "FindMin":
+                Console.WriteLine(min);
+                break;
+            case "FindMean":
+                if (count == 0) {
+                } else {
+                    double mean = sum / count;
+                    Console.WriteLine(mean);
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid mode.");
+                break;
+        }
+    }
+}
